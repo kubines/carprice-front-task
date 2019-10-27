ef2ac6b641a339ad7269f8c694f8c7d31bdd3b35
kubines <cubax@mail.ru>
2019-10-27 21:24:08 +0700

    upd App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import Provider from 'State/Provider'
+
+import Panel from './Panel'
+import ItemList from './ItemList'
+import ItemCreate from './ItemCreate'
+
+import './style.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Provider>
+			<div className='container'>
+				<Panel />
+				<ItemList />
+				<ItemCreate />
+			</div>
+		</Provider>
+	)
 }
 
-export default App;
+export default App
